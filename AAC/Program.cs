9e3adec93f12a98f6f542b96bdb0d5f822b8300e
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-23 22:48:32 +0330

    feat: add main switch
    The main switch is the switch which should handles the main menu

--- AAC/Program.cs
+++ AAC/Program.cs
@@ -53,6 +53,8 @@ namespace CodeWars
                             }
                         )
                 );
+
+                switch (partNumbers[chosenStr])
                 {
 
                 }
