94166a113ccf8605e74f76e42bf4c23b807e77b6
Reza Shahmoradi <rezashahmoradi.81@outlook.com>
2024-02-24 15:59:34 +0330

    feat: combine commands
    Because of changing the shell for running commands, for each calling of runcommand method user should give administrator access so multiple calling cause multiple message of taking access which is annoying so i combined them together.

--- AAC/Program.cs
+++ AAC/Program.cs
@@ -224,68 +224,57 @@ namespace CodeWars
                         );
                         break;
                     case 2:
-                        RunCommand("netsh interface ipv4 set address name=\"Wi-Fi\" static 192.168.1.100 255.255.255.0 192.168.1.1");
-                        RunCommand("netsh interface ipv4 set dns name=\"Wi-Fi\" static 8.8.8.8");
-                        RunCommand("netsh interface ipv4 add dns name=\"Wi-Fi\" 4.2.2.4 index=2");
+                        RunCommand(
+                            "netsh interface ipv4 set address name=\"Wi-Fi\" static 192.168.1.100 255.255.255.0 192.168.1.1; netsh interface ipv4 set dns name=\"Wi-Fi\" static 8.8.8.8; netsh interface ipv4 add dns name=\"Wi-Fi\" 4.2.2.4 index=2;"
+                        );
                         break;
                     case 3:
-                        RunCommand("netsh interface ipv4 set address name=\"Wi-Fi\" static 192.168.85.62 255.255.255.0 192.168.85.1");
-                        RunCommand("netsh interface ipv4 set dns name=\"Wi-Fi\" static 8.8.8.8");
-                        RunCommand("netsh interface ipv4 add dns name=\"Wi-Fi\" 4.2.2.4 index=2");
+                        RunCommand(
+                            "netsh interface ipv4 set address name=\"Wi-Fi\" static 192.168.85.62 255.255.255.0 192.168.85.1; netsh interface ipv4 set dns name=\"Wi-Fi\" static 8.8.8.8; netsh interface ipv4 add dns name=\"Wi-Fi\" 4.2.2.4 index=2;"
+                        );
                         break;
                     case 4:
-                        RunCommand("netsh interface ipv4 set address name=\"Ethernet\" static 192.168.85.61 255.255.255.0 192.168.85.1");
-                        RunCommand("netsh interface ipv4 set dns name=\"Ethernet\" static 8.8.8.8");
-                        RunCommand("netsh interface ipv4 add dns name=\"Ethernet\" 4.2.2.4 index=2");
+                        RunCommand(
+                            "netsh interface ipv4 set address name=\"Ethernet\" static 192.168.85.61 255.255.255.0 192.168.85.1; netsh interface ipv4 set dns name=\"Ethernet\" static 8.8.8.8; netsh interface ipv4 add dns name=\"Ethernet\" 4.2.2.4 index=2;"
+                        );
                         break;
                     case 5:
-                        RunCommand("netsh interface ipv4 set address name=\"Ethernet\" source=dhcp");
-                        RunCommand("netsh interface ipv4 set dnsservers name=\"Ethernet\" source=dhcp");
+                        RunCommand(
+                            "netsh interface ipv4 set address name=\"Ethernet\" source=dhcp; netsh interface ipv4 set dnsservers name=\"Ethernet\" source=dhcp;"
+                        );
                         break;
                     case 6:
-                        RunCommand("netsh interface ipv4 set dns name=\"Wi-Fi\" static 185.51.200.2");
-                        RunCommand("netsh interface ipv4 add dns name=\"Wi-Fi\" 178.22.122.100 index=2");
+                        RunCommand(
+                            "netsh interface ipv4 set dns name=\"Wi-Fi\" static 185.51.200.2; netsh interface ipv4 add dns name=\"Wi-Fi\" 178.22.122.100 index=2;"
+                        );
                         break;
                     case 7:
-                        RunCommand("netsh interface ipv4 set dns name=\"Ethernet\" static 185.51.200.2");
-                        RunCommand("netsh interface ipv4 add dns name=\"Ethernet\" 178.22.122.100 index=2");
+                        RunCommand(
+                            "netsh interface ipv4 set dns name=\"Ethernet\" static 185.51.200.2; netsh interface ipv4 add dns name=\"Ethernet\" 178.22.122.100 index=2;"
+                        );
                         break;
                     case 8:
-                        RunCommand("netsh interface ipv4 set dns name=\"Wi-Fi\" static 8.8.8.8");
-                        RunCommand("netsh interface ipv4 add dns name=\"Wi-Fi\" 4.2.2.4 index=2");
+                        RunCommand(
+                            "netsh interface ipv4 set dns name=\"Wi-Fi\" static 8.8.8.8; netsh interface ipv4 add dns name=\"Wi-Fi\" 4.2.2.4 index=2;"
+                        );
                         break;
                     case 9:
-                        RunCommand("netsh interface ipv4 set dns name=\"Ethernet\" static 8.8.8.8");
-                        RunCommand("netsh interface ipv4 add dns name=\"Ethernet\" 4.2.2.4 index=2");
+                        RunCommand(
+                            "netsh interface ipv4 set dns name=\"Ethernet\" static 8.8.8.8; netsh interface ipv4 add dns name=\"Ethernet\" 4.2.2.4 index=2;"
+                        );
                         break;
                     case 10:
-                        Console.WriteLine("\n=========================================================================================");
-                        RunCommand("ipconfig /flushdns");
-                        Console.WriteLine("\n=========================================================================================");
-                        RunCommand("ipconfig /release");
-                        Console.WriteLine("\n=========================================================================================");
-                        RunCommand("ipconfig /renew");
-                        Console.WriteLine("\t\tPress any key to exit");
-                        Console.ReadKey();
-
+                        RunCommand("ipconfig /flushdns; ipconfig /release; ipconfig /renew");
                         break;
                     case 11:
-                        Console.WriteLine("\n=========================================================================================");
-                        RunCommand("ping -a -n 2 8.8.8.8");
-                        Console.WriteLine("\n=========================================================================================");
-                        RunCommand("ping -a -n 2 soft98.ir");
-                        Console.WriteLine("\t\tPress any key to exit");
-                        Console.ReadKey();
-
+                        RunCommand(
+                            "ping -a -n 2 soft98.ir; echo \" \" \"press any key to exit\"; $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');"
+                        );
                         break;
                     case 12:
-                        Console.WriteLine("\n=========================================================================================");
-                        RunCommand("ping -a -n 2 youtube.com");
-                        Console.WriteLine("\n=========================================================================================");
-                        RunCommand("ping -a -n 2 pornhub.com");
-                        Console.WriteLine("\t\tPress any key to exit");
-                        Console.ReadKey();
-
+                        RunCommand(
+                            "ping -a -n 2 youtube.com; echo \" \" \"press any key to exit\"; $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');"
+                        );
                         break;
                 }
                 Console.Clear();
